c8d07923b57e5d631a06d075ca444d19bc3c4558
Gautam Raj <gautamraj996@gmail.com>
2024-01-19 08:36:03 +0530

    Project01 - new component created and little bit of styling done

---
+++ Project01/AppPro.tsx
@@ -0,0 +1,26 @@
+import {Text, View, StyleSheet, useColorScheme} from 'react-native';
+
+function AppPro(): JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark'
+
+  return (
+      <View style={styles.container}>
+        <Text style={isDarkMode?styles.whiteText:styles.darkText}>App Pro</Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    whiteText:{
+        color:'#fff',
+    },
+    darkText:{
+        color:'#000',
+    }
+})
+export default AppPro;
